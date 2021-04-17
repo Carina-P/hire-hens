1b54170befd258dcf9261ff5fc8f9f947b7ba370
Carina-P <carina.palmqvist@telia.com>
2021-04-17 13:09:09 +0000

    Ready to deploy to heroku

--- hire_hens/settings.py
+++ hire_hens/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 import dj_database_url
 from pathlib import Path
 
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gw12#x#%dxj0yl64yxkn@q879w^mznf+!0eazl4j5i&id=j=sm'
+SECRET_KEY = os.environ.get('SECRET_KEY', '') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['hire-hens.herokuapp.com', 'localhost']
 
@@ -74,17 +75,17 @@ WSGI_APPLICATION = 'hire_hens.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
-DATABASES = {
-    'default': dj_database_url.parse('postgres://rwepdrkdrjhtrt:eca8256905f3918a6c9d509d5114c96e39dd74e8fc3de9abb77a8b1e8705efa5@ec2-108-128-104-50.eu-west-1.compute.amazonaws.com:5432/d5esudetn5nk4p')
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
