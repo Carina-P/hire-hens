13bf11b891a0e8a3325a297c8a3f02bc14d32827
Carina-P <carina.palmqvist@telia.com>
2021-05-08 15:22:25 +0000

    Built skeleton templates: base_product.html and products_by_category.html

--- products/models.py
+++ products/models.py
@@ -4,9 +4,6 @@ from django.db import models
 
 
 class Category(models.Model):
-    
-    class Meta:
-        verbose_category_plural = "Categories"
 
     category = models.CharField(max_length=20)
     buyable = models.BooleanField()
