c052ec355e9bd058da2f9cd5c3df22f5c4d1162e
Carina-P <carina.palmqvist@telia.com>
2021-05-08 12:12:39 +0000

    Added the product app with models Product and Category

--- faq/models.py
+++ faq/models.py
@@ -6,3 +6,6 @@ from django.db import models
 class Faq(models.Model):
     question = models.CharField(max_length=254, null=False, blank=False)
     answer = models.CharField(max_length=500, null=False, blank=False)
+
+    def __str__(self):
+        return self.question
