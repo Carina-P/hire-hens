3013d161484e96b3e73f5c70869d1d45202685a5
Carina-P <carina.palmqvist@telia.com>
2021-05-12 16:13:23 +0000

    Checkout: Created app and models

---
+++ checkout/models.py
@@ -0,0 +1,116 @@
+import uuid
+from django.db import models
+from django.db.models import Sum
+
+from products.models import Product
+
+# Create your models here.
+
+
+class Order(models.Model):
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    country = models.CharField(max_length=40, null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    order_date = models.DateTimeField(auto_now_add=True)
+    grand_total = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, default=0
+        )
+    delivery_date = models.DateTimeField(null=True, blank=True)
+
+    def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        """
+        Update grand total each time a line item is added,
+        accounting for delivery costs.
+        """
+        buy_total = self.buyitems.aggregate(
+            Sum('buyitem_total')
+            )['buyitem_total__sum']
+
+        rent_total = self.rentitems.aggregate(
+            Sum('rentitem_total')
+            )['rentitem_total__sum']
+
+        self.grand_total = buy_total + rent_total
+        self.save()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it hasn't been set already.
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
+
+class OrderBuyItem(models.Model):
+    order = models.ForeignKey(
+        Order,
+        null=False,
+        blank=False,
+        on_delete=models.CASCADE,
+        related_name='buyitems'
+        )
+    product = models.ForeignKey(
+        Product, null=False, blank=False, on_delete=models.CASCADE
+        )
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    buyitem_total = models.DecimalField(
+        max_digits=6, decimal_places=2, null=False, blank=False, editable=False
+        )
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the buyitem total
+        and update the order total.
+        """
+        self.buyitem_total = self.product.buying_price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.product
+
+
+class OrderRentalItem(models.Model):
+    order = models.ForeignKey(
+        Order,
+        null=False,
+        blank=False,
+        on_delete=models.CASCADE,
+        related_name='rentalitems'
+        )
+    product = models.ForeignKey(
+        Product, null=False, blank=False, on_delete=models.CASCADE
+        )
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    months = models.IntegerField(null=False, blank=False, default=0)
+    rentalitem_total = models.DecimalField(
+        max_digits=6, decimal_places=2, null=False, blank=False, editable=False
+        )
+    end_of_rental = (models.DateTimeField(null=True, blank=True))
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the rentalitem total
+        and update the order total.
+        """
+        self.rentalitem_total = self.product.rental_price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.product
--- hire_hens/settings.py
+++ hire_hens/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'faq',
     'products',
     'cart',
+    'checkout',
 
     # Other
     'storages',
