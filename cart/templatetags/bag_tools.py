a3531a18c8796a1ae176c40dd3de8c85795f48e4
Carina-P <carina.palmqvist@telia.com>
2021-05-12 09:41:56 +0000

    Cart: Added another template tag: calc_cart_total

--- cart/templatetags/bag_tools.py
+++ cart/templatetags/bag_tools.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.filter(name='calc_subtotal')
 def calc_subtotal(price, quantity):
     return price * quantity
+
+
+@register.filter(name='calc_cart_total')
+def calc_cart_total(buy_total, rental_total):
+    return buy_total + rental_total
