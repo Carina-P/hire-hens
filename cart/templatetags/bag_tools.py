e7158114a6464bd22758dd2f38c40a8ca72954fd
Carina-P <carina.palmqvist@telia.com>
2021-05-12 11:34:48 +0000

    Cart: Added calc_rent_subtotal tag and did some styling of cart

--- cart/templatetags/bag_tools.py
+++ cart/templatetags/bag_tools.py
@@ -11,3 +11,8 @@ def calc_subtotal(price, quantity):
 @register.filter(name='calc_cart_total')
 def calc_cart_total(buy_total, rental_total):
     return buy_total + rental_total
+
+
+@register.simple_tag(name='calc_rent_subtotal')
+def calc_rent_subtotal(price, quantity, months):
+    return price * quantity * int(months)
