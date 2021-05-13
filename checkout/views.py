b2f3eab214ff3ad81ba0b8bfc93c3debef7d3d15
Carina-P <carina.palmqvist@telia.com>
2021-05-13 06:37:40 +0000

    Checkout: Added initial templates

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 # Create your views here.
+
+
+def checkout(request):
+    cart = request.session.get('cart', {})
+     cart_rental = request.session.get('cart_rental', {})
+
+    if not cart and not cart_rental:
+        messages.error(request, "There is nothing in your cart at the moment")
+        return redirect('home')
+    
+    context = {
+        'order_form': OrderForm(), 
+    }
+
+    return render(request, 'checkout/checkout.html', context)
