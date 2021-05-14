4674745a3795dc6e457abf9e7ad13aaf25f401cc
Carina-P <carina.palmqvist@telia.com>
2021-05-14 12:11:21 +0000

    Checkout: Created a stripe intent

--- checkout/views.py
+++ checkout/views.py
@@ -11,6 +11,10 @@ import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+
     cart = request.session.get('cart', {})
     cart_rental = request.session.get('cart_rental', {})
 
@@ -24,11 +28,21 @@ def checkout(request):
     total_rental = current_rental_cart["total_rental"]
     # Stripe only accepts integer as amount to charge
     stripe_total = round((total + total_rental) * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            To the devloper: must be set in the environment.')
+
 
     context = {
         'order_form': OrderForm(),
-        'stripe_public_key': 'pk_test_51IX166JKlvgFF1ppZywzGgXRLOalVYSY6XpGTCwLoQx1BJK36QkRFZPzzceMo7noBEjnva5vEQCnz2fP1lCfmulp00Ieu64lFN',
-        'client_secret': 'test_client_secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, 'checkout/checkout.html', context)
