122ee5fa8a96336c11e079b4954b62dc05d63e94
Carina-P <carina.palmqvist@telia.com>
2021-05-14 10:14:39 +0000

    Checkout: Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
 
     context = {
         'order_form': OrderForm(),
+        'stripe_public_key': 'pk_test_51IX166JKlvgFF1ppZywzGgXRLOalVYSY6XpGTCwLoQx1BJK36QkRFZPzzceMo7noBEjnva5vEQCnz2fP1lCfmulp00Ieu64lFN',
+        'client_secret': 'test_client_secret',
     }
 
     return render(request, 'checkout/checkout.html', context)
