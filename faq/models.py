18cfb16adf8f42378e4e61787f5e0d35909130fc
Carina-P <carina.palmqvist@telia.com>
2021-05-23 18:56:27 +0000

    FAQ: Question textfield instead of charfield

--- faq/models.py
+++ faq/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Faq(models.Model):
     question = models.CharField(max_length=254, null=False, blank=False)
-    answer = models.CharField(max_length=500, null=False, blank=False)
+    answer = models.TextField(max_length=500, null=False, blank=False)
     objects = models.Manager()
 
     def __str__(self):
