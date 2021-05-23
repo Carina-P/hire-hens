fa61df0bd4e946ee81085f9598a90a2df1b1af2a
Carina-P <carina.palmqvist@telia.com>
2021-05-23 08:55:38 +0000

    FAQ: Styling of input

--- faq/forms.py
+++ faq/forms.py
@@ -6,3 +6,19 @@ class FaqForm(forms.ModelForm):
     class Meta:
         model = Faq
         fields = ['question', 'answer']
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'question': 'Question',
+            'answer': 'Answer',
+        }
+
+        self.fields['question'].widget.attrs['placeholder'] = f'{placeholders["question"]} *'
+        self.fields['question'].widget.attrs['class'] = 'input-style'
+
+        self.fields['answer'].widget.attrs['placeholder'] = f'{placeholders["answer"]} *'
+        self.fields['answer'].widget.attrs['class'] = 'input-style'
