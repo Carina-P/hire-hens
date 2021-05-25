1e677e498ea40e42b676839084c8121e6d16e8c9
Carina-P <carina.palmqvist@telia.com>
2021-05-25 18:00:51 +0000

    Profile: Styled

--- profiles/forms.py
+++ profiles/forms.py
@@ -31,6 +31,6 @@ class UserProfileForm(forms.ModelForm):
                     placeholder = placeholders[field]
                 self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = (
-                'border-black rounded-0 profile-form-input'
+                'border-black profile-form-input mb-2'
             )
             self.fields[field].label = False
